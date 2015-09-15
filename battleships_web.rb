90bbce608b5f84673c53c0d97b5722d41c7b8401
danielstpaul <danielstpaul@hotmail.com>
2015-09-15 16:49:40 +0100

    Update working V1

--- battleships_web.rb
+++ battleships_web.rb
@@ -1,7 +1,7 @@
-require 'sinatra/base'
+require 'sinatra/base' #controller
 
 class BattleshipsWeb < Sinatra::Base
-  set :views, proc { File.join(root, '..', 'views') }
+  set :views, proc { File.join(root, 'views') }
 
   get '/' do
     erb :index
@@ -11,6 +11,13 @@ class BattleshipsWeb < Sinatra::Base
     @name = params[:name]
     erb :new_game
   end
+
+  # post '/board' do
+  #   session[:name] = params[:name]
+  #   @name = session[:name]
+  #   # @board = Board.new
+  #   erb :board
+  # end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
