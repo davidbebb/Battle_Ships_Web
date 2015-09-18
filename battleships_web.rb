1b0993a188e565eb22ec3a455ff5626340eb88a6
davidbebb <davidbebb@gmail.com>
2015-09-18 07:56:30 +0100

    placing a 5 unit ship with get/post works!!!! :)

--- battleships_web.rb
+++ battleships_web.rb
@@ -10,6 +10,7 @@ class BattleshipsWeb < Sinatra::Base
   register Sinatra::Partial
   set :views, proc { File.join(root, 'views') }
   set :partial_template_engine, :erb
+  enable :sessions
   # enable :partial_underscores
 
 
@@ -19,7 +20,8 @@ class BattleshipsWeb < Sinatra::Base
 
   get '/new_game' do
     @name = params[:name]
-    @board = Board.new(Cell).grid
+    @board = Board.new(Cell)
+    session[:board] = @board
     output = ""
     output << partial( :new_game )
     output << partial( :print_board )
@@ -27,8 +29,18 @@ class BattleshipsWeb < Sinatra::Base
   end
 
   get '/add_ships' do
-    @fleet = [Ship.aircraft_carrier, Ship.battleship, Ship.destroyer, Ship.submarine, Ship.patrol_boat ]
-    erb :add_ships
+    @ship = Ship.aircraft_carrier
+    session[:ship] = @ship
+    @board=session[:board]
+    output = ""
+    output << partial( :add_ships )
+    output << partial( :print_board )
+    output
+  end
+
+  post '/add_ships' do
+    session[:board].place(session[:ship], params[:coords].to_sym, params[:orientation].to_sym)
+    redirect '/add_ships'
   end
   # start the server if ruby file executed directly
   run! if app_file == $0
